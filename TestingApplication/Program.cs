7b0fce29f9f36391fa6e2c9e7838ed58484adead
HP <abdulraufsiddiqui007@gmail.com>
2021-08-16 09:12:46 -0700

    added sum method feature

--- TestingApplication/Program.cs
+++ TestingApplication/Program.cs
@@ -4,9 +4,18 @@ namespace TestingApplication
 {
     class Program
     {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            var result = program.Sum(5, 5);
+
+            Console.WriteLine(result);
         }
     }
 }
+
