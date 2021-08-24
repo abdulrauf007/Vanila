30632b24881cc64b63700aff77cc950e8f07b9df
HP <abdulraufsiddiqui007@gmail.com>
2021-08-24 20:17:42 +0500

    Calculator example

---
+++ TestingApplication/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingApplication
+{
+    public class Calculator
+  
+    {
+        public int Add(int Num1, int Num2)
+        {
+
+            return Num1 + Num2;
+
+        }
+
+        public int Sub(int Num1, int Num2)
+        {
+            return Num1 - Num2;
+        }
+
+        public int Multiplication(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+
+        public int Division(int Num1, int Num2)
+        {
+            if (Num2 == 0)
+                return 0;
+            return Num1 / Num2;
+        }
+    }
+}
--- TestingApplication/Program.cs
+++ TestingApplication/Program.cs
@@ -5,16 +5,22 @@ namespace TestingApplication
     class Program
     {
 
-        public int Sum(int a, int b)
-        {
-            return a + b;
-        }
+     
         static void Main(string[] args)
         {
-            Program program = new Program();
-            var result = program.Sum(5, 5);
+       
 
-            Console.WriteLine(result);
+            Calculator Cal = new Calculator();
+            var addResult = Cal.Add(10, 2);
+            var subResult = Cal.Sub(100, 35);
+            var mulResult = Cal.Multiplication(23, 6);
+            var divResult = Cal.Division(32, 2);
+
+
+            Console.WriteLine(addResult);
+            Console.WriteLine(subResult);
+            Console.WriteLine(mulResult);
+            Console.WriteLine(divResult);
         }
     }
 }
