6114ecdfc1c1812201f2d8b002f3060e2a493edb
HP <abdulraufsiddiqui007@gmail.com>
2021-09-02 21:05:58 +0500

    InheritanceBase

---
+++ TestingApplication/Base.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingApplication
+{
+    class Base
+    {
+        public DateTime CreateDateTime { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+        private String Createdby { get; set; }
+        private String updatedby { get; set; }
+    }
+
+    class student : Base
+    {
+        public String StdName { get; set; }
+        public String FName { get; set; }
+        public String School { get; set; }
+    }
+
+    class teacher : Base
+    {
+        public String TeacherName { get; set; }
+        public String FName { get; set; }
+        public String Qulification { get; set; }
+
+       
+        
+    }
+
+     
+}
